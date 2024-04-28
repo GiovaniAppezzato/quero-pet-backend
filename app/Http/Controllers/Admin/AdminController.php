0a04c11c4e2e7cac041115b7a7a8ece07dfd1bc5
Giovani Appezzato <giovani.apztt@gmail.com>
2024-04-28 13:11:57 -0300

    feat: added "AdminController"

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Enums\UserTypeEnum;
 use App\Models\User;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return UserResource::collection(User::all());
+        return UserResource::collection(User::admins()->get());
     }
 
     public function show(User $user)
@@ -21,6 +22,28 @@ class AdminController extends Controller
 
     public function store(Request $request)
     {
-        // ...
+        $validated = $request->validated();
+
+        // ** Create a new user record.
+        $user = User::create([
+            ...$validated->user,
+            'password'     => bcrypt($request->password),
+            'user_type_id' => UserTypeEnum::ADMIN->value
+        ])->admin()->create($validated->admin);
+
+        return new UserResource($user);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $validated = $request->validated();
+
+        // ** Update the user record.
+        $user->update($validated->user);
+
+        // ** Update the admin record.
+        $user->admin->update($validated->admin);
+
+        return new UserResource($user);
     }
 }
