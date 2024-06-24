7418267b8e3c8019aae76baf19c9b0634d61f5a3
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-06-24 02:23:50 -0300

    feat: OrderController for both Adopter and Ong, new functions

---
+++ app/Http/Controllers/Adopter/OrderController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Adopter;
+
+use App\Models\Pet;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $adopter = Auth::user()->adopter;
+        $orders = Order::where('adopter_id', $adopter->id)->get();
+
+        return response()->json([
+            'orders' => $orders,
+            'adopter' => $adopter
+        ], 200);
+    }
+
+    public function store($petId) //Q: Or Request?
+    {
+        return DB::transaction(function () use ($petId) {
+            $pet = Pet::findOrFail($petId);
+            $user = Auth::user();
+
+            $order = Order::create([
+                'status' => 'pending',
+                'pet_id' => $pet->id,
+                'adopter_id' => $user->adopter->id,
+                'ong_id' => $pet->ong->id,
+            ]);
+
+            //Q: Return again?
+            return response()->json([
+                'order' => $order,
+                'success' => true
+           ]);
+       });
+    }
+}
---
+++ app/Http/Controllers/Ong/OrderController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Ong;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        //Get all the orders that have this ong id
+
+        $ongId = Auth::user()->ong->id;
+
+        $orders = Order::where('ong_id', $ongId)->get();
+
+        return response()->json([
+            'orders' => $orders
+        ], 200);
+    }
+
+    public function show($orderId)
+    {
+        $order = Order::find($orderId);
+        $adopter = $order->adopter;
+
+        return response()->json([
+            'order' => $order,
+            'adopter' => $adopter
+        ], 200);
+    }
+
+    public function acceptOrder($orderId)
+    {
+        return DB::transaction(function () use ($orderId) {
+            $order = Order::findOrFail($orderId);
+            $order->update([
+                'status' => 'accepted',
+                'adopted_at' => now(),
+            ]);
+        });
+    }
+
+    public function cancelOrder($orderId)
+    {
+        return DB::transaction(function () use ($orderId) {
+            $order = Order::findOrFail($orderId);
+            $order->update([
+                'status' => 'canceled',
+                'canceled_at' => now(),
+            ]);
+        });
+    }
+}
